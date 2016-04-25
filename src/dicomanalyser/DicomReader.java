8b4aaf02c1ec3dc4af174ac9e775960afbe38299
orlov <evgenyorlov1@gmail.com>
2016-04-25 03:42:59 +0300

    Fix Otsu histogram

--- src/dicomanalyser/DicomReader.java
+++ src/dicomanalyser/DicomReader.java
@@ -11,10 +11,12 @@ import com.pixelmed.dicom.CompressedFrameDecoder;
 import com.pixelmed.dicom.DicomException;
 import java.util.ArrayList;
 import java.io.File;
+import java.util.Collections;
 import com.pixelmed.display.ConsumerFormatImageMaker;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -33,7 +35,7 @@ public class DicomReader {
         //file();
         //imagePane();
         //convert();
-        imagePixel();
+        otsuThresholding();
     }
     
     
@@ -112,34 +114,115 @@ public class DicomReader {
     }
  
     
-    private static void imagePixel() {
+    private static void otsuThresholding() {
+        //will have issues with 2 or 1 colored image
         try {
             String path = System.getProperty("user.dir") + "/dicomImigies/IM-0001-0001.dcm";
             SourceImage sImg = new SourceImage(path); 
-            ArrayList<int[]> histogram = new ArrayList<int[]>();            
             BufferedImage bImg = sImg.getBufferedImage(); 
-            
-            
-            //create histogram                                    
+            ArrayList<Object[]> histogram = new ArrayList<Object[]>(); //histogram  
+            float[] withinClassVariance = new float[2]; //holds minimum class variance and index
+                        
+            //create histogram
+            ArrayList numOfPixels = new ArrayList(); //number of pixels
+            ArrayList numOfColors = new ArrayList(); //number of grey colors
             for(int i=0; i<bImg.getHeight(); i++) {
-                for(int j=0; j<bImg.getWidth(); j++) {    
-                    int[] pixelStat = new int[2]; //histogram pixel value, statistic
-                    pixelStat[0] = bImg.getRGB(j, i);
-                    pixelStat[1] = 1;
-                    histogram.add(pixelStat);                                     
+                for(int j=0; j<bImg.getWidth(); j++) {
+                    numOfPixels.add(bImg.getRGB(j, i));
+                    if(!numOfColors.contains(bImg.getRGB(j, i))) {
+                        numOfColors.add(bImg.getRGB(j, i));
+                    }
                 }
-            }           
-            System.out.println(histogram.size());
-            //Otsu thresholding
-            for(int i=0; i<histogram.size(); i++) {
-                System.out.println(histogram.get(i)[0]);
+            }            
+            //histogram itself
+            //DO I NEED TO SORT GREY COLORS?
+            for(int i=0; i<numOfColors.size(); i++) {
+                Object[] temp = new Object[2];
+                temp[0] = numOfColors.get(i); //pixel rgb                
+                temp[1] = Collections.frequency(numOfPixels, numOfColors.get(i)); //frequency
+                Color c = new Color((int)temp[0], true);
+                System.out.println(c.getRed());
+                System.out.println(c.getGreen());
+                System.out.println(c.getBlue());
+                System.out.println((int)temp[0]);
+                System.out.println("----------");
+                histogram.add(temp);                
             }
-        } catch(Exception e) {
-            System.out.println("Error:" + e);
-        }
+            
+            //otsu algorythm, asume that there is distinct foreground and background
+            for(int T=0; T<histogram.size()-1; T++) {
+                //Background                
+                int weightB_temp = 0;
+                int meanB_temp = 0;                
+                for(int j=0; j<=T; j++) {
+                    weightB_temp += (int) histogram.get(j)[1];                    
+                    meanB_temp += j*(int) histogram.get(j)[1];
+                }                               
+                float WeightB = (float)weightB_temp/numOfPixels.size(); //pixels weight                
+                float MeanB = (float)meanB_temp/weightB_temp; //pixels mean                                                                            
+                //background variance
+                float varianceB_temp = 0; 
+                for(int j=0; j<=T; j++) {                                        
+                    varianceB_temp += (j-MeanB)*(j-MeanB)*(int) histogram.get(j)[1];
+                }
+                float VarianceB = varianceB_temp/weightB_temp;               
+                
+                //Foreground
+                int weightF_temp = 0;
+                int meanF_temp = 0;
+                for(int j=T+1; j<histogram.size(); j++) {                 
+                    weightF_temp += (int) histogram.get(j)[1];
+                    meanF_temp += j*(int) histogram.get(j)[1];
+                }                
+                float WeightF = (float)weightF_temp/numOfPixels.size();
+                float MeanF = (float)meanF_temp/weightF_temp;  
+                //foreground variance
+                float varianceF_temp = 0;
+                for(int j=T+1; j<histogram.size(); j++) {
+                    varianceF_temp += (j-MeanF)*(j-MeanF)*(int) histogram.get(j)[1];                    
+                }
+                float VarianceF = varianceF_temp/weightF_temp;                
+                
+                //Within Class Variance                
+                if(T!=0) {                    
+                    float withinClassVariance_temp = WeightB*VarianceB + WeightF*VarianceF;
+                    if(withinClassVariance_temp < withinClassVariance[1]) {
+                        withinClassVariance[0] = T; //minimum T index
+                        withinClassVariance[1] = withinClassVariance_temp; //WCV value                        
+                    }                    
+                } else {                        
+                    withinClassVariance[0] = T; //minimum T index
+                    withinClassVariance[1] = WeightB*VarianceB + WeightF*VarianceF; //WCV value                    
+                }                
+            }
+            
+            //doesn't work
+            BufferedImage finalImage = new BufferedImage(bImg.getWidth(), 
+                    bImg.getHeight(), BufferedImage.TYPE_INT_RGB);
+            
+            for(int i=0; i<bImg.getHeight(); i++) {
+                for(int j=0; j<bImg.getWidth(); j++) {
+                    int rgb = bImg.getRGB(j, j);    
+                    
+                    for(int position=0; position<histogram.size(); position++) {                        
+                        if(rgb==(int)histogram.get(position)[0]) {
+                            if(position<=(int)withinClassVariance[0])
+                                rgb = Color.BLACK.getRGB();
+                            else
+                                rgb = Color.WHITE.getRGB();
+                        }
+                    }
+                    
+                    finalImage.setRGB(j, i, rgb);
+                }
+            }
+            File outputFile = new File("image.jpg");
+            ImageIO.write(finalImage, "jpg", outputFile);
+            System.out.println("Done");
+        } catch(Exception e) {System.out.println("Error:" + e);}                
     }
     
-    
+   
     private static void fileSort(String[] files) {
         for(int i = 0; i < files.length; i++) {
             //sort file path's before thresholding(time tags are epsent)
